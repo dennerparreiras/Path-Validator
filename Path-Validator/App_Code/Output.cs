e480d477ce241da64e07b98483ffd29e7a8d2fb8
Denner Parreiras <rennedenner@icloud.com>
2018-10-20 23:24:07 -0300

    Code sliced to new classes

---
+++ Path-Validator/App_Code/Output.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Path_Validator.App_Code
+{
+    static class Output
+    {
+        public static void Error(string p_ErrorMessage = "", bool p_IgnoreMessageBox = false)
+        {
+            string v_ErrorMessage, v_ErrorTitle;
+
+            v_ErrorMessage = (!String.IsNullOrEmpty(p_ErrorMessage)) ? p_ErrorMessage : @"Houve uma falha ao realizar o procedimento.\r\nFavor revisar as configurações de execução.";
+            v_ErrorTitle = @"Alerta";
+
+            Print(@"[ERROR] | " + v_ErrorTitle + @": " + v_ErrorMessage);
+
+            if (!p_IgnoreMessageBox)
+            {
+                MessageBox.Show(v_ErrorMessage, v_ErrorTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void Print(string p_Message = "Action executed.")
+        {
+            Console.WriteLine(p_Message);
+        }
+    }
+}
---
+++ Path-Validator/App_Code/Statistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Path_Validator.App_Code
+{
+    class Statistics
+    {
+        public int EmptyFolder { get; set; }
+        public int FolderWithFiles { get; set; }
+        public int TotalFilesFound { get; set; }
+        public List<String> EmptyFolderList { get; set; }
+        public List<String> FolderWithFilesList { get; set; }
+        public String FinalReport { get; set; }
+
+        public Statistics()
+        {
+            this.EmptyFolder = 0;
+            this.FolderWithFiles = 0;
+            this.TotalFilesFound = 0;
+            this.EmptyFolderList = new List<string>();
+            this.FolderWithFilesList = new List<string>();
+        }
+    }
+}
