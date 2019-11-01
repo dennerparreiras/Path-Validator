4a95b25169b957ed72ee5a3ebf0a06b46d87678f
Denner Parreiras <dennerparreiras@gmail.com>
2019-10-31 22:04:25 -0300

    Commit 2

--- Path-Validator/MainScreen.Designer.cs
+++ Path-Validator/MainScreen.Designer.cs
@@ -304,7 +304,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Path_Validator.Properties.Resources.Network_Background;
+            this.BackgroundImage = global::Path_Validator.Properties.Resources.topo_fundo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(940, 598);
             this.Controls.Add(this.Logo);
